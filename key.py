cc39861d877a3f2c7563308d7be5cc20d9778a5a
tanpv <phamtan500@gmail.com>
2018-06-03 23:56:23 +0700

    start with Key class

--- key.py
+++ key.py
@@ -5,6 +5,7 @@
 		- create private key
 		- get public point
 		- get public key in compressed format
+		- get public key in uncompressed format
 		- get address from public key
 		
 		support wif format
@@ -30,5 +31,22 @@
 """
 
 import os
+import binascii
 
-print(len(os.urandom(32)))
+
+class Key():
+
+	"""
+		support 2 case:
+			- if user specify private_key --> create key with this private key
+			- if user not specify private_key --> auto create a random 32 bytes number for private key
+	"""
+	def __init__(self, private_key=None):
+		if private_key == None:
+			self.private_key = os.urandom(32)
+			# convert from byte stream to readable format
+			self.private_key_readable = binascii.hexlify(self.private_key)
+		else:
+			self.private_key_readable = private_key
+			self.private_key = binascii.unhexlify(private_key.encode('ascii'))
+
